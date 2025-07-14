441d034d5c414a036df4a0d487fbee16a50f2203
Hao.Nguyen <hao.nguyen@gravityglobal.com>
2025-07-14 23:05:45 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const app = express();
+
+// Init middleware
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+// Init db
+
+// Init routes
+
+// Handle Errors
+
+module.exports = app;
